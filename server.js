e8e945136265d29f2f8c141db2f8224460339052
JPO <jpo@JPO-MacBook-Pro.local>
2020-03-18 18:13:16 +0100

    Add endpoint for incoming messages

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
-var messages = ""
+var recievedMessages = ""
+var messageStatuses = ""
 var notifications = ""
 
 app.get("/", function (request, response) {
-  response.send("<h1>Messages</h1>\n"+messages+ "\n<h1>Notifications</h1>"+notifications);
+  response.send("<h1>Recieved messages</h1>\n"+recievedMessages+"<h1>Send messages statuses</h1>\n"+messageStatuses+"\n<h1>Notifications</h1>"+notifications);
+});
+
+app.post("/", function (request, response) {
+  console.log('Incoming message: ' + JSON.stringify(request.body));
+  recievedMessages += "<p>" + JSON.stringify(request.body) + "</p>";
+  response.sendStatus(200);
 });
 
 app.post("/messages", function (request, response) {
-  console.log('Incoming message: ' + JSON.stringify(request.body));
-  messages += "<p>" + JSON.stringify(request.body) + "</p>";
+  console.log('Incoming message status: ' + JSON.stringify(request.body));
+  messageStatuses += "<p>" + JSON.stringify(request.body) + "</p>";
   response.sendStatus(200);
 });
 
